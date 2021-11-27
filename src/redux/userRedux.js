44111d89299408335238fbb5c8459ca7c2efaed0
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-11-27 20:14:30 +0700

    client coshopy

---
+++ src/redux/userRedux.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    currentUser: null,
+    users:[],
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
+    loginFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+
+    //register
+    addUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    addUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.users.push(action.payload);
+    },
+    addUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure,
+  addUserStart,
+  addUserSuccess,
+  addUserFailure, } = userSlice.actions;
+export default userSlice.reducer;
